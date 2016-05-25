720bf16302014c83017cfb2e846acc6c2dfe40ce
mitsuhide <m.mitsuhide@amatelus.com>
2016-05-25 16:27:13 +0900

    trash file deleted

--- node_modules/stringifier/test/rjsconfig.js
+++
@@ -1,9 +0,0 @@
-var require = {
-    paths: {
-        stringifier: "../build/stringifier",
-        "type-name": "../node_modules/type-name/build/type-name",
-        assert: '../build/assert',
-        mocha: "../bower_components/mocha/mocha",
-        requirejs: "../bower_components/requirejs/require"
-    }
-};
--- node_modules/stringifier/test/travis_es6.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ "$TRAVIS_NODE_VERSION" != "0.10" ]
-then
-    $(npm bin)/mocha --harmony test/es6_test.es6
-fi
