b192fcf6865d8fd5eec00dbc931c5f0c83494f54
mitsuhide <m.mitsuhide@amatelus.com>
2016-06-26 11:28:06 +0900

    added attributes of geometry

--- src/Goml/createGeometry.ts
+++ src/Goml/createGeometry.ts
@@ -1,50 +1,69 @@
 import * as THREE from "three";
 
+const attrsPool = [];
+const attrsCorePool = [];
+const indexAttrsCorePool = [];
+
 export default ( value ) => {
 
-    if ( value.type === "Buffer" ) {
-      const geometry = new THREE.BufferGeometry;
-      for ( let key in value.attrs ) {
-        if ( key ) { // if使わないとlintに怒られる
-          geometry.addAttribute( key, new THREE.BufferAttribute( new Float32Array( value.attrs[ key ] ),
-            key === "index" ? 1 : key === "uv" ? 2 : 3 ) );
-        }
-      }
+  let geometry;
 
+  if ( value.type === "Buffer" ) {
+    geometry = new THREE.BufferGeometry;
+  } else {
+    geometry = new THREE[ value.type + "Geometry" ](
+      value.value[ 0 ],
+      value.value[ 1 ],
+      value.value[ 2 ],
+      value.value[ 3 ],
+      value.value[ 4 ],
+      value.value[ 5 ],
+      value.value[ 6 ],
+      value.value[ 7 ] );
+  }
 
-      return geometry;
+  if ( value.attrs ) {
+    let tmp = value.attrs;
+    let length = tmp.position.length / 3;
+    let index = attrsPool.indexOf( tmp );
+
+    if ( index !== -1 ) {
+      geometry.attributes = attrsCorePool[ index ];
     } else {
-      const geometry = new THREE[ value.type + "Geometry" ](
-        value.value[ 0 ],
-        value.value[ 1 ],
-        value.value[ 2 ],
-        value.value[ 3 ],
-        value.value[ 4 ],
-        value.value[ 5 ],
-        value.value[ 6 ],
-        value.value[ 7 ] );
 
-      /*if ( value.faceVertexUvs ) {
-        value.faceVertexUvs.forEach( ( array, i0 ) => {
-          geometry.faceVertexUvs[ i0 ] = geometry.faceVertexUvs[ i0 ] || [];
-          array.forEach( ( uvs, i1 ) => {
-            geometry.faceVertexUvs[ i0 ][ i1 ] = geometry.faceVertexUvs[ i0 ][ i1 ] || [];
-            uvs.forEach( ( uv, i2 ) => {
-                geometry.faceVertexUvs[ i0 ][ i1 ][ i2 ] = new THREE.Vector2( uv[ 0 ], uv[ 1 ] );
-            });
-          });
-        });
-      }*/
-
-      if ( value.attrs ) {
-        for ( let key in value.attrs ) {
-          if ( key ) {
-            geometry.addAttribute( key, new THREE.BufferAttribute( new Float32Array( value.attrs[ key ] ),
-              key === "index" ? 1 : key === "uv" ? 2 : 3 ) );
-          }
+      for ( let key in tmp ) {
+        if ( key === "index" ) {
+          geometry.setIndex( Array.isArray( tmp[ key ] ) ? new THREE.Uint16Attribute( tmp[ key ], 1 ) : tmp[ key ] );
+        } else {
+          geometry.addAttribute( key, Array.isArray( tmp[ key ] ) ?
+            new THREE.Float32Attribute( tmp[ key ], tmp[ key ].length / length ) : tmp[ key ] );
         }
       }
 
-      return geometry;
+      attrsPool.push( tmp );
+      attrsCorePool.push( geometry.attributes );
+      indexAttrsCorePool.push( geometry.getIndex() );
+
+      Object.defineProperty( tmp, "needsUpdate", {
+        get: function () {
+          return attrsCorePool[ attrsPool.indexOf( this ) ].position.needsUpdate;
+        },
+        set: function ( bool ) {
+          let index = attrsPool.indexOf( this );
+          for ( let key in this ) {
+            if ( key === "index" ) {
+              indexAttrsCorePool[ index ].array.set( this[ key ] );
+              indexAttrsCorePool[ index ].needsUpdate = true;
+            } else {
+              attrsCorePool[ index ][ key ].array.set( this[ key ] );
+              attrsCorePool[ index ][ key ].needsUpdate = true;
+            }
+          }
+        },
+      });
     }
+
+  }
+
+  return geometry;
 };
